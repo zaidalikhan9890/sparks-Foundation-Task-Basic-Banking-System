c1d6275007d5cc438880f355ebb103b658666340
zaidalikhan9890 <108212295+zaidalikhan9890@users.noreply.github.com>
2022-06-26 12:30:59 +0530

    Add files via upload

---
+++ National-Banking-System-main/README.md
@@ -0,0 +1,13 @@
+# Basic-Banking-System
+
+***I have created a Basic Banking System as an intern for the Sparks Foundation.*** <br>
+#GRIPFEBRUARY22
+
+Intern @ The Sparks Foundation
+Task 1: Basic Banking System<br>
+The Basic Banking System website function is to transfer money from one person's account to another person's account.
+<br>
+<br>
+<b><i>FrontEnd: HTML, CSS, JS</i></b><br>
+<b><i>Backend: PHP</i></b><br>
+<b><i>Database: MySQL</i></b>
---
+++ National-Banking-System-main/index.php
@@ -0,0 +1,36 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body style="background-color: black;">
+  <header>
+  <?php
+  include 'navbar.php';
+  ?></header>
+<main>
+  <style>
+  body{
+    background-image: url('img/xyz.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    width: 100%;
+    background-position: 300px 70px;
+  }
+</style>
+<h1 style = "position:relative; left:80px; top:80px; color:white;  font-family: Candara;"><b>W</b></h1>
+<h1 style = "position:relative; left:90px; top:80px; color:white;  font-family: Candara;"><b>E</b></h1>
+<h1 style = "position:relative; left:90px; top:80px; color:white;  font-family: Candara;"><b>L</b></h1>
+<h1 style = "position:relative; left:85px; top:80px; color:white;  font-family: Candara;"><b>C</b></h1>
+<h1 style = "position:relative; left:85px; top:80px; color:white;  font-family: Candara;"><b>O</b></h1>
+<h1 style = "position:relative; left:80px; top:80px; color:white;  font-family: Candara;"><b>M</b></h1>
+<h1 style = "position:relative; left:88px; top:80px; color:white;  font-family: Candara;"><b>E</b></h1>
+    </main>
+  </body>
+</html>
